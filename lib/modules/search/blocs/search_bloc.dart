7cc856efd9035822ecb0d953950f0b123ec964bd
Sean K <1682772+sk92129@users.noreply.github.com>
2025-01-26 22:16:48 -0800

    no anchor

--- lib/modules/search/blocs/search_bloc.dart
+++ lib/modules/search/blocs/search_bloc.dart
@@ -1,13 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:searchsimple/modules/search/blocs/search_event.dart';
 import 'package:searchsimple/modules/search/blocs/search_state.dart';
+import 'package:searchsimple/modules/search/data/itunes_item.dart';
+import 'package:searchsimple/modules/search/util/search_itune.dart';
 
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   SearchBloc() : super(EmptySearchState()) {
     on<SearchingEvent>(
-      (event, emit) {
-        emit(state.copyWith(data: event.data));
+      (event, emit) async {
+        Iterable<ITunesItem> data = [];
+
+        data = await searchITunes("Major Tom");
+        List<String> titles = data.map((e) => e.trackName).toList();
+
+        emit(state.copyWith(data: titles));
       },
     );
   }
